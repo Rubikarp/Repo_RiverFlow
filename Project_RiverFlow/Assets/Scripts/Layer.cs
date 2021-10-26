515beee0bab7644c5e957cf7bebeb6967c38abb2
Arthur Deleye <a.deleye@rubika-edu.com>
2021-10-26 17:29:47 +0200

    Layer created

---
+++ Project_RiverFlow/Assets/Scripts/Layer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer : MonoBehaviour
+{
+    [Header("reférence")]
+    public GameGrid grid;
+
+    [Header("parameter")]
+    public Tile[,] tiles;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
