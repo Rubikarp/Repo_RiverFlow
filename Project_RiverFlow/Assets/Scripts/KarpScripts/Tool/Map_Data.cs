569b381160624096bba2926600d8f34b86db6fe0
Arthur Deleye <a.deleye@rubika-edu.com>
2021-12-16 20:27:59 +0100

    Wallah le ptit tool qui met bien arrive

---
+++ Project_RiverFlow/Assets/Scripts/KarpScripts/Tool/Map_Data.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Map_new", menuName = "LevelDesign/New_Map")]
+
+public class Map_Data : ScriptableObject
+{
+    public Vector2Int gridSize = new Vector2Int(8,8);
+    public TileType[] gridFieldType = new TileType[8*8];
+
+#if UNITY_EDITOR
+    public Color[] tileColor = new Color[5 /*System.Enum.GetValues(typeof(TileType)).Length*/]
+    {Color.magenta, Color.green, Color.red, Color.yellow, Color.grey };
+
+    public TileType selectedType;
+#endif
+}
