7ef392dfb52b4d8a90e5a5a5bbbf41ae554955f4
Edouard <e.murat@rubika-edu.com>
2022-01-20 17:17:31 +0100

    Interactions des boutons (pb: pas de TimeManager)

--- Project_RiverFlow/Assets/Scripts/Core/UI/PauseUI.cs
+++ Project_RiverFlow/Assets/Scripts/Core/UI/PauseUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseUI : MonoBehaviour
 {
@@ -35,5 +36,23 @@ public class PauseUI : MonoBehaviour
         }
     }
 
+    public void Resume()
+    {
+        isPaused = false;
+        pauseUI.SetActive(false);
+
+        Time.timeScale = 1;
+        timeManager.gameTimeSpeed = 1;
+    }
+
+    public void Options()
+    {
+        // Ouvrir l'UI des Options
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("_MainMenu");
+    }
 
 }
